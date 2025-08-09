fc3e6819613c2a06379acb4b5f29186a0fc723dd
RinKM250613 <rebeca.yllanes.ch@gmail.com>
2025-08-08 22:49:07 -0500

    estilos done

--- angular/src/app/vendedor/vendedor-routing.module.ts
+++ angular/src/app/vendedor/vendedor-routing.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { VendedorLayoutComponent } from "../layout/vendedor-layout/vendedor-layout.component";
+import { InicioComponent } from "./inicio/inicio.component";
+import { VentasComponent } from "./ventas/ventas.component";
+import { HistorialComponent } from "./historial/historial.component";
 
 const routes: Routes = [
   {
-    path:'',
-    component:VendedorLayoutComponent,
-    children:[
-
+    path: 'vendedor',
+    component: VendedorLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'inicio',
+        pathMatch: 'full'
+      },
+      {
+        path: 'inicio',
+        component: InicioComponent
+      },
+      {
+        path: 'ventas',
+        component: VentasComponent
+      },
+      {
+        path: 'historial',
+        component: HistorialComponent
+      }
     ],
   },
 ];
