552d8a0c89dbcec3541163b69716a2399f648c71
rebeYllanes25 <rebeca.yllanes.ch@gmail.com>
2025-08-18 21:38:26 -0500

    modulo vendedor grilla completada

--- angular/src/app/vendedor/ventas/ventas.component.ts
+++ angular/src/app/vendedor/ventas/ventas.component.ts
@@ -1,29 +1,135 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarroService } from '../service/carro.service';
 import { Venta } from '../../shared/model/venta.model';
 import { Producto } from '../../shared/model/producto.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { DetalleVenta } from '../../shared/model/detalleVenta.model';
+import { Usuario } from '../../shared/model/usuario.model';
 
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './ventas.component.html',
   styleUrl: './ventas.component.css'
 })
-export class VentasComponent {
+export class VentasComponent implements OnInit {
+
+  productos: Producto[] = [];
+  productosSeleccionados: DetalleVenta[] = [];
+  carrito: DetalleVenta[] = [];
+
+  usuarioPrecargado: Usuario = {
+    idUsuario: 3,
+    nombres: 'Juan',
+    apeMaterno: 'Meza',
+    apePaterno: 'Gonzales',
+    correo: 'juan.gonzales@example.com',
+    clave: 'clave123',
+    nroDocumento: '34567890',
+    direccion: 'Jr. Perú 789',
+    distrito: { idDistrito: 3, nombre: 'El Agustino' },
+    telefono: '987654323',
+    rol: { idRol: 3, descripcion: 'Vendedor' },
+    fechaRegistro: new Date().toISOString(),
+    estado: true
+  };
+
+  venta: Venta = {
+    idVenta: 0,
+    usuario: this.usuarioPrecargado,
+    total: 0,
+    detalles: []
+  };
+
   constructor(private carroService: CarroService) { }
 
-  agregarAlCarrito(producto: Producto) {
-    this.carroService.agregarProducto(producto, 1);
+  ngOnInit(): void {
+    this.cargarProductos();
   }
 
-  comprar() {
-    this.carroService.finalizarVenta(1).subscribe({
+  cargarProductos() {
+    this.carroService.listarProductosActivos().subscribe({
+      next: data => this.productos = data,
+      error: err => console.error('Error cargando productos', err)
+    });
+  }
+
+  agregarSeleccionado(producto: Producto) {
+    const existente = this.productosSeleccionados.find(d => d.producto.idProducto === producto.idProducto);
+
+    if (existente) {
+      existente.cantidad++;
+      existente.subTotal = existente.cantidad * existente.producto.precio;
+    } else {
+      this.productosSeleccionados.push({
+        producto,
+        cantidad: 1,
+        subTotal: producto.precio
+      });
+    }
+  }
+
+
+
+  aumentarCantidad(item: DetalleVenta) {
+    if (item.cantidad < item.producto.stock) {
+      item.cantidad++;
+      item.subTotal = item.cantidad * item.producto.precio;
+    } else {
+      alert('Stock insuficiente');
+    }
+  }
+
+  disminuirCantidad(item: DetalleVenta) {
+    if (item.cantidad > 1) {
+      item.cantidad--;
+      item.subTotal = item.cantidad * item.producto.precio;
+    } else {
+      this.productosSeleccionados = this.productosSeleccionados.filter(
+        d => d.producto.idProducto !== item.producto.idProducto
+      );
+    }
+  }
+
+  pasarAlCarrito() {
+    this.productosSeleccionados.forEach(p => this.carroService.agregarProducto(p.producto, p.cantidad));
+    this.productosSeleccionados = [];
+    this.carrito = this.carroService.getItems();
+  }
+
+
+  calcularTotalCarrito(): number {
+    return this.carrito.reduce((acc, d) => acc + d.subTotal, 0);
+  }
+
+  finalizarVenta() {
+    if (this.carroService.getItems().length === 0) {
+      alert('El carrito está vacío');
+      return;
+    }
+
+    const venta: Venta = {
+      idVenta: 0,
+      usuario: this.usuarioPrecargado,
+      detalles: this.carroService.getItems(),
+      total: this.carroService.getTotal()
+    };
+
+    console.log('Venta que se envía:', venta);
+
+    this.carroService.finalizarVenta(venta).subscribe({
       next: resp => {
-        console.log('Venta realizada:', resp);
+        console.log('Venta finalizada', resp);
+        alert('Venta realizada con éxito');
         this.carroService.limpiarCarrito();
+        this.carrito = [];
       },
-      error: err => console.error('Error en venta', err)
+      error: err => {
+        console.error('Error en la venta', err);
+        alert('Error al finalizar venta');
+      }
     });
   }
 }
