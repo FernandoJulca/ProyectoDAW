ed55cda6c500fba61db792c909bfc021c303131e
FernandoJulca <ferjose222810@gmail.com>
2025-08-23 14:08:48 -0500

    Agregando login incompleto

--- angular/src/app/auth/auth.module.ts
+++ angular/src/app/auth/auth.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 
+
+
 @NgModule({
+ 
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    
   ]
 })
 export class AuthModule {}
